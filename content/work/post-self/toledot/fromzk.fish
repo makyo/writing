4ede54f7f5e2df0d74dfc4e8d26c5487bbbfb5d1
Madison Scott-Clary <mscottclary@nvidia.com>
2021-09-04 12:28:48 -0700

    Book stuff

--- content/work/post-self/toledot/fromzk.fish
+++ content/work/post-self/toledot/fromzk.fish
@@ -1,6 +1,14 @@
-for i in (fd '0.*' ~/sparkleup/zk/writing/post-self/qoheleth/)
-     set o (echo $i | sed -e 's/.\+qoheleth\///')
-     echo "$o"
-     head -n 1 $i | sed -e 's/%title /# /' > src/$o
-     tail -n +3 $i >> src/$o
+for i in (fd '0.*' ~/sparkleup/zk/writing/post-self/toledot/)
+     set o (echo $i | sed -e 's/.\+toledot\///')
+     set d (echo $o | sed -e 's/[0-9]\+.md//')
+     set t (echo $o | sed -e 's/\.md/.tex/')
+     echo "$o $d"
+     if not test -d src/$d
+         mkdir -p src/$d
+     end
+     if not test -d content/$d
+         mkdir -p content/$d
+     end
+     cp $i src/$o
+    pandoc -f markdown -t latex src/$o --wrap=none --top-level-division=chapter | sed -e 's/\\chapter/\\chapter*/' | sed -e 's/---/—/g' > content/$t; \
  end
