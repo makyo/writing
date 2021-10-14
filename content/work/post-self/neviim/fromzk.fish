8958cc11e56d9b626b274d5d0817ed3f6c509cd7
Madison Scott-Clary <mscottclary@nvidia.com>
2021-10-13 23:34:50 -0700

    Toledot, Qoheleth updates

---
+++ content/work/post-self/neviim/fromzk.fish
@@ -0,0 +1,14 @@
+for i in (fd '0.*' ~/sparkleup/zk/writing/post-self/neviim/)
+     set o (echo $i | sed -e 's/.\+neviim\///')
+     set d (echo $o | sed -e 's/[0-9]\+.md//')
+     set t (echo $o | sed -e 's/\.md/.tex/')
+     echo "$o $d"
+     if not test -d src/$d
+         mkdir -p src/$d
+     end
+     if not test -d content/$d
+         mkdir -p content/$d
+     end
+     cp $i src/$o
+    pandoc -f markdown -t latex src/$o --wrap=none --top-level-division=chapter | sed -e 's/\\chapter/\\chapter*/' | sed -e 's/---/—/g' > content/$t; \
+ end
