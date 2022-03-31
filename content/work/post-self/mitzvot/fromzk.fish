6cae344416cf05728559c4c1c70c2bbb5d93b71b
Madison Scott-Clary <makyo@drab-makyo.com>
2022-03-30 17:20:44 -0700

    Mitzvot, Nevi'im edits

---
+++ content/work/post-self/mitzvot/fromzk.fish
@@ -0,0 +1,14 @@
+for i in (fd '0.*' ~/sparkleup/zk/writing/post-self/mitzvot/)
+     set o (echo $i | sed -e 's/.\+mitzvot\///')
+     set d (echo $o | sed -e 's/[0-9x]\+.md//')
+     set t (echo $o | sed -e 's/\.md/.tex/')
+     echo "$o $d"
+     if not test -d src/$d
+         mkdir -p src/$d
+     end
+     if not test -d content/$d
+         mkdir -p content/$d
+     end
+     cp $i src/$o
+    pandoc -f markdown -t latex src/$o --wrap=none --top-level-division=chapter | sed -e 's/\\chapter/\\chapter*/' | sed -e 's/---/—/g' > content/$t; \
+ end
