c2dd7ce66d6950c87551db8a0b3720a3d6631d20
Madison Scott-Clary <makyo@drab-makyo.com>
2022-09-01 13:02:23 -0700

    mitzvot, WtWWY

--- content/work/post-self/mitzvot/fromzk.fish
+++ content/work/post-self/mitzvot/fromzk.fish
@@ -12,3 +12,17 @@ for i in (fd '0.*' ~/sparkleup/zk/writing/post-self/mitzvot/)
      cp $i src/$o
     pandoc -f markdown -t latex src/$o --wrap=none --top-level-division=chapter | sed -e 's/\\chapter/\\chapter*/' | sed -e 's/---/—/g' > content/$t; \
  end
+for i in (fd '0.*' ~/sparkleup/zk/writing/post-self/selected-letters/)
+     set o (echo $i | sed -e 's/.\+selected-letters\///')
+     set d (echo $o | sed -e 's/[0-9x]\+.md//')
+     set t (echo $o | sed -e 's/\.md/.tex/')
+     echo "$o $d"
+     if not test -d src/letters/$d
+         mkdir -p src/letters$d
+     end
+     if not test -d content/letters/$d
+         mkdir -p content/letters/$d
+     end
+     cp $i src/$o
+    pandoc -f markdown -t latex src/$o --wrap=none --top-level-division=section | sed -e 's/\\section/\\section*/' | sed -e 's/---/—/g' > content/letters/$t; \
+ end
